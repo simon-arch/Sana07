2bea3a7f17895aa960c04471c250eff0f6e14d3e
Артем Ващенко <ipz231_vai@student.ztu.edu.ua>
2024-01-21 13:08:48 +0200

    Created base class (Product) with basic get/set properties. Added abstract Display() method

--- Abstraction/ClassLibrary.cs
+++ Abstraction/ClassLibrary.cs
@@ -1,5 +1,29 @@
 ﻿namespace Abstraction;
 public abstract class Product
 {
-
+    protected string name;
+    protected double price;
+    protected int count;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public double Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+    public int Count
+    {
+        get { return count; }
+        set { count = value; }
+    }
+    protected Product(string name, double price, int count)
+    {
+        Name = name;
+        Price = price;
+        Count = count;
+    }
+    public abstract void Display();
 }
