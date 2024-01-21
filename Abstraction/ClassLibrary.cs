756f7fbe79c069758a56db6a4eda56f06f375070
Артем Ващенко <ipz231_vai@student.ztu.edu.ua>
2024-01-21 13:58:07 +0200

    Implemented inherited (Laptop) class with basic get/set properties. Created separate object for testing purposes

--- Abstraction/ClassLibrary.cs
+++ Abstraction/ClassLibrary.cs
@@ -27,3 +27,44 @@ public abstract class Product
     }
     public abstract void Display();
 }
+
+public class Laptop : Product
+{
+    protected string processor;
+    protected string videocard;
+    protected int storage;
+    protected int ram;
+    public string Processor 
+    {
+        get { return processor; }
+        set { processor = value; }
+    }
+    public string Videocard 
+    { 
+        get { return videocard; }
+        set { videocard = value; }
+    }
+    public int Storage 
+    { 
+        get { return storage; }
+        set { storage = value; }
+    }
+    public int Ram 
+    {
+        get { return ram; }
+        set { ram = value; }
+    }
+    public Laptop(string name, double price, int count, string processor, string videocard, int storage, int ram)
+        : base(name, price, count)
+    {
+        Processor = processor;
+        Videocard = videocard;
+        Storage = storage;
+        Ram = ram;
+    }
+    public override void Display()
+    {
+        Console.WriteLine($"Категорія: Ноутбук\nНазва: {name}\nЦіна: {price} грн\nКількість: {count} шт.\n" +
+            $"Модель процесора: {processor}\nМодель відеокарти: {videocard}\nОб'єм накопичувача: {storage} гб\nОЗУ: {ram} гб");
+    }
+}
--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -3,5 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Laptop laptop = new Laptop("Lenovo IdeaPad Gaming 3 15ARH7", 34999, 1, "Ryzen 5 6600H", "GeForce RTX 3050", 512, 16);
+        laptop.Display();
     }
 }
